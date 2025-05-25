ad10d4b4f67ab011e961d19d007576828230f7d7
Saikat Sardar <saikatsardar5@gmail.com>
2025-05-25 16:12:19 +0530

    project init 🚀

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import { HeaderProps } from "@/lib/types";
+import Link from "next/link";
+import ThemeToggleButton from "../ui/ThemeToggleButton";
+
+const Header = ({ appName }: HeaderProps) => {
+  return (
+    <header
+      className="border-b shadow"
+      aria-label="app-header">
+      <div className="container mx-auto flex items-center justify-between px-6 py-3">
+        <Link href={"/"}>
+          <h1
+            className="text-2xl font-semibold"
+            aria-label="App Name">
+            {appName}
+          </h1>
+        </Link>
+
+        <nav className="flex items-center gap-4">
+          <Link href={"/"}>Home</Link>
+
+          <ThemeToggleButton />
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
