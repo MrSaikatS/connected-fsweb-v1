524734f8799bdd2c27bf746c9343b94efd8d08c8
Saikat Sardar <saikatsardar5@gmail.com>
2025-05-27 11:19:56 +0530

    Update MobileMenu.tsx

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -3,30 +3,28 @@ import Link from "next/link";
 
 const MobileMenu = () => {
   return (
-    <>
-      <div className="fixed right-0 bottom-0 left-0 z-50 grid grid-cols-3 place-items-center border-t py-2">
-        <Link
-          href={"/"}
-          className="grid place-items-center">
-          <LayoutDashboard />
-          <span className="text-xs">Feed</span>
-        </Link>
+    <div className="fixed right-0 bottom-0 left-0 z-50 grid grid-cols-3 place-items-center border-t py-2">
+      <Link
+        href={"/"}
+        className="grid place-items-center">
+        <LayoutDashboard />
+        <span className="text-xs">Feed</span>
+      </Link>
 
-        <Link
-          href={"/"}
-          className="grid place-items-center">
-          <SquarePen />
-          <span className="text-xs">Create</span>
-        </Link>
+      <Link
+        href={"/"}
+        className="grid place-items-center">
+        <SquarePen />
+        <span className="text-xs">Create</span>
+      </Link>
 
-        <Link
-          href={"/"}
-          className="grid place-items-center">
-          <UserRound />
-          <span className="text-xs">Profile</span>
-        </Link>
-      </div>
-    </>
+      <Link
+        href={"/"}
+        className="grid place-items-center">
+        <UserRound />
+        <span className="text-xs">Profile</span>
+      </Link>
+    </div>
   );
 };
 
