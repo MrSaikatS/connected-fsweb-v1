540bd02f822372dccbe70c0f21dc0aa7489bc869
Saikat Sardar <saikatsardar5@gmail.com>
2025-05-27 11:34:18 +0530

    React-toastify added

--- src/components/Header/AppHeader.tsx
+++ src/components/Header/AppHeader.tsx
@@ -5,29 +5,27 @@ import AppMenu from "./AppMenu";
 
 const AppHeader = () => {
   return (
-    <>
-      <header
-        className="fixed top-0 right-0 left-0 z-50 border-b shadow"
-        aria-label="app-header">
-        <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-3">
-          <Link href={"/"}>
-            <h1
-              className="text-2xl font-semibold"
-              aria-label="App Name">
-              ConnectEd
-            </h1>
-          </Link>
+    <header
+      className="fixed top-0 right-0 left-0 z-50 border-b shadow"
+      aria-label="app-header">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-3">
+        <Link href={"/"}>
+          <h1
+            className="text-2xl font-semibold"
+            aria-label="App Name">
+            ConnectEd
+          </h1>
+        </Link>
 
-          <nav className="flex items-center gap-4">
-            <AppMenu />
+        <nav className="flex items-center gap-4">
+          <AppMenu />
 
-            <LogoutButton />
+          <LogoutButton />
 
-            <ThemeToggleButton />
-          </nav>
-        </div>
-      </header>
-    </>
+          <ThemeToggleButton />
+        </nav>
+      </div>
+    </header>
   );
 };
 
